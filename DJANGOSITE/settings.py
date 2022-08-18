49d4f6be82c622fbbbe5a4a65b9927fc0253d52d
unknown <h1kudz>
2022-08-18 20:55:23 +0900

    deploy

--- DJANGOSITE/settings.py
+++ DJANGOSITE/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-aza5t-4w!qvmrf9tvq%a1(xxadz7i(f$i%arc0oj_d)!tqv$oq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -124,6 +124,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'DJANGOSITE/static'),
 ]
+import dj_database_url
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
